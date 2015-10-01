91cb2eb18196b9a26c6912ef66710319bcd4fd37
Garry A Offord <droffo@yahoo.com>
2015-09-30 22:37:08 -0500

    Added extra stuff.

---
+++ bash_aliases.sh
@@ -0,0 +1,21 @@
+alias l='ls -F'
+alias la='ls -Fa'
+alias ll='ls -Fl'
+alias lla='ls -Fla'
+
+alias md='mkdir -v'
+alias mp='mkdir -vp'
+
+alias cp='/bin/cp -iv'
+alias mv='/bin/mv -iv'
+alias rm='/bin/rm -iv'
+
+alias rp='realpath'
+alias t="tree --charset=ansi -F -I '.git/*'"
+
+alias eba='vim $HOME/src/dotfiles/bash_aliases.sh'
+alias ebf='vim $HOME/src/dotfiles/bash_functions.sh'
+alias ebr='vim $HOME/src/dotfiles/bashrc.sh'
+alias rba='source $HOME/src/dotfiles/bash_aliases.sh'
+alias rbf='source $HOME/src/dotfiles/bash_functions.sh'
+alias rbr='source $HOME/src/dotfiles/bashrc.sh'
