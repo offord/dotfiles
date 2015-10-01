fba5264133cd45f08a8868f1cb843cbd628120d5
Garry A Offord <droffo@yahoo.com>
2015-09-30 23:15:46 -0500

    Added more aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -11,7 +11,7 @@ alias mv='/bin/mv -iv'
 alias rm='/bin/rm -iv'
 
 alias rp='realpath'
-alias t="tree --charset=ansi -F -I '.git/*'"
+alias t='tree --charset=ansi -F -I .git'
 
 alias eba='vim $HOME/src/dotfiles/bash_aliases.sh'
 alias ebf='vim $HOME/src/dotfiles/bash_functions.sh'
@@ -19,3 +19,11 @@ alias ebr='vim $HOME/src/dotfiles/bashrc.sh'
 alias rba='source $HOME/src/dotfiles/bash_aliases.sh'
 alias rbf='source $HOME/src/dotfiles/bash_functions.sh'
 alias rbr='source $HOME/src/dotfiles/bashrc.sh'
+
+alias purge='find . \( -name \*\~ -o -name .\*\~ \) -exec rm -v {} \;'
+
+alias ga='git add'
+alias gcf='git commit -f'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gs='git status'
